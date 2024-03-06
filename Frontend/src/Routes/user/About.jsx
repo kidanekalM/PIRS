bf1e3b2c84d44c81ab344deb8d20b0f850ee2fde
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-03-06 15:20:48 +0300

    updated about

--- Frontend/src/Routes/user/About.jsx
+++ Frontend/src/Routes/user/About.jsx
@@ -2,9 +2,10 @@ import UNav from "../../components/user_dashboard/user_nav"
 export default function About(){
     return<>
     <UNav/>
+    <br/>
     <h3 style={{color: "black", textAlign:"center"}}>Public Reporting System</h3>
     <br />
-    <p style={{color: "black", textAlign:"center"}}>This application is made to report problems related to any of the copanies that we work with to help with fast support and </p>
+    <p style={{color: "black", textAlign:"center"}}>This application is made to report problems related to any of the companies that we work with to help with fast support and maintainance and reduce risk and inconvinience. </p>
     <br />
     <p style={{color: "black", textAlign:"center"}}>Contact: +2519</p>
     </>
