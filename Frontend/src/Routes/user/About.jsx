1c1fa237f6545b8cd8f37771c40683277aa571a1
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-03-07 18:32:28 +0300

    formattinf

--- Frontend/src/Routes/user/About.jsx
+++ Frontend/src/Routes/user/About.jsx
@@ -7,11 +7,10 @@ export default function About(){
     <br />
     <p style={{color: "black", textAlign:"center" , padding:"2rem"}}>
         <p>
-
-        This app is like a one-stop-shop for keeping track of any issues with our partner companies. It’s been carefully built to speed up support and maintenance tasks. This way, we can quickly deal with any potential problems, making life easier for our partners and customers.
+            This app is like a one-stop-shop for keeping track of any issues with our partner companies. It’s been carefully built to speed up support and maintenance tasks. This way, we can quickly deal with any potential problems, making life easier for our partners and customers.
         </p>
         <p>
-        The app shows our dedication to smooth service and building solid relationships with our partner companies. It’s all about being proactive in solving problems, not just reacting to them, but also trying to prevent them.
+            The app shows our dedication to smooth service and building solid relationships with our partner companies. It’s all about being proactive in solving problems, not just reacting to them, but also trying to prevent them.
         </p>
         <p>
         In short, this app isn’t just a tool - it’s a strategic buddy in keeping our businesses running smoothly. It’s a protector of our shared interests and a major player in our mission to deliver top-notch service. It’s our secret weapon to make sure our operations are always running as slick and efficient as possible.
