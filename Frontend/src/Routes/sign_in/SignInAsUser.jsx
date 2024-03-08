ecef7f6cf05eba4f96763f2dfbcec5ec018a5f33
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-03-08 18:08:21 +0300

    Update SignInAsUser.jsx

--- Frontend/src/Routes/sign_in/SignInAsUser.jsx
+++ Frontend/src/Routes/sign_in/SignInAsUser.jsx
@@ -73,7 +73,7 @@ export default function SignInAsUser({redirectUrl}) {
     localStorage.setItem('AuthInfo',true);
     window.location.href = ((redirectUrl==null)|| (redirectUrl==""))?
       role === 'User'
-        ? '/udashboard'
+        ? 'Udashboard'
         : role === 'Contractor'
         ? '/ContDashboard'
         : '/Company':`/${redirectUrl}`;
